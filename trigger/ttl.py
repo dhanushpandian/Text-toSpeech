8e1cb44671f4fa93e689a8b292cd8ad12b973be8
Dhanush Pandian D <dhanushdpandian@gmail.com>
2024-01-24 21:05:24 +0530

    Update ttl.py

--- trigger/ttl.py
+++ trigger/ttl.py
@@ -27,18 +27,18 @@ if __name__ == "__main__":
     message = sys.argv[1]
     text_to_speech(message)
 
-# import sys
-# import pyttsx3
+import sys
+import pyttsx3
 
-# def text_to_speech(message):
-#     engine = pyttsx3.init()
-#     engine.say(message)
-#     engine.runAndWait()
+def text_to_speech(message):
+    engine = pyttsx3.init()
+    engine.say(message)
+    engine.runAndWait()
 
-# if __name__ == "__main__":
-#     if len(sys.argv) != 2:
-#         print("Usage: python3 ttl.py <message>")
-#         sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 ttl.py <message>")
+        sys.exit(1)
 
-#     message = sys.argv[1]
-#     text_to_speech(message)
+    message = sys.argv[1]
+    text_to_speech(message)
